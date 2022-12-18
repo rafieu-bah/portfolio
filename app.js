762c8958888db9dad2a93c83af962f3c7755ec36
rafieubah <rafieubah@gmail.com>
2022-12-18 23:59:33 +0000

    ree

---
+++ app.js
@@ -0,0 +1,17 @@
+"use strict"
+
+const projectOneBtn = document.querySelector(".btn1")
+const projectTwoBtn = document.querySelector(".btn2")
+const projectThreeBtn = document.querySelector(".btn3")
+
+projectOneBtn.addEventListener("click", () =>{
+  alert("This project is underdevelopmet, please check later")
+});
+
+projectTwoBtn.addEventListener("click", () =>{
+  alert("This project is underdevelopmet, please check later")
+});
+
+projectThreeBtn.addEventListener("click", () =>{
+  alert("This project is underdevelopmet, please check later")
+});
